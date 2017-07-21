d770b207e74664992dd03ffb29090408b5a0346e
odeckgaming <odeckgaming@gmail.com>
2017-07-20 21:54:51 -0400

    Add files via upload

---
+++ server_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" > "1" ]; then
+	if [[ $1 == -v* ]]; then	#if vanilla
+		java -Xms8G -Xmx8G -jar minecraft_server.1.10.2.jar nogui
+	elif [[ $1 == -i* ]]; then	#install forge
+		java -jar forge-1.10.2-12.18.3.2316-installer.jar --installServer
+	fi
+else java -Xms8G -Xmx8G -jar forge-1.10.2-12.18.3.2316-universal.jar nogui
+fi
